1cbdcabacd216e4538d73f8f17fde4ddbadebc59
Adrian Weitkemper <adi004@web.de>
2019-09-03 16:15:35 +0200

    take out heldenDesOlymp

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ app.use(bodyParser.json());
 const monopoly = require('./Monopoly/index.js');
 app.use('/monopoly/', monopoly);
 
-const heldenDesOlymp = require('./helden_des_olymp/index.js');
-app.use('/heldenDesOlymp/', heldenDesOlymp);
+// const heldenDesOlymp = require('./helden_des_olymp/index.js');
+// app.use('/heldenDesOlymp/', heldenDesOlymp);
 
 const davincicode = require('./davincicode/index.js');
 app.use('/davincicode/', davincicode);
