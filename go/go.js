72b37dd2d7a90ad739fc2150488ad74a5de28ec3
Adrian Weitkemper <adi004@web.de>
2020-01-06 19:06:32 +0100

    adding go ("umzingeln")

---
+++ go/go.js
@@ -0,0 +1,54 @@
+var reihenfolge = [];
+
+class go {
+  constructor(broadcastReceiver, sendReceiver) {
+    this.broadcast = broadcastReceiver;
+    this.send = sendReceiver;
+  }
+  onJoin(client) {
+  //  console.log(`${client.sessionId} joined.`);
+    let newPlayer = {
+      id: client.sessionId,
+      client: client,
+    };
+    if (this.player1 == null) this.player1 = newPlayer;
+    else if (this.player2 == null) this.player2 = newPlayer;
+    if (!(this.player1 == null) && !(this.player2 == null)) {
+      console.log("player1 + player2: " + this.player1.id + " - " + this.player2.id);
+  console.log("Spiel beginnen");
+reihenfolge = [this.player1.id, this.player2.id];
+this.broadcast({
+type: "Reihenfolge",
+data: reihenfolge
+});
+this.send(this.player1.client, {
+  type: "name",
+  data: reihenfolge[0]
+});
+this.send(this.player2.client, {
+  type: "name",
+  data: reihenfolge[1]
+});
+  }
+}
+  onMessage(client, data) {
+console.log(data.message.type + " - " +      data.message.data[data.message.sender] + " - " + data.message.requiredPoints);
+        if (data.message.type == "points" && data.message.data[data.message.sender] >=  data.message.requiredPoints)  {
+      this.broadcast(data.message.sender + " wins!");
+    }
+    else this.broadcast(data.message);
+  }
+  onLeave(client) {
+    if (!(this.player1 == null || this.client == undefined)) {
+  if (this.client.sessionId == this.player1.id) {
+      this.player1 = null;
+  }
+  else {
+    this.player2 = null;
+  }
+}
+
+}
+}
+
+module.exports = go;
