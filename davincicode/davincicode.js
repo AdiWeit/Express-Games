32c69030027d522055d3e00fb483fb2db395a82c
Adrian Weitkemper <adi004@web.de>
2020-04-15 17:46:21 +0200

    Joker (vote decides) added

--- davincicode/davincicode.js
+++ davincicode/davincicode.js
@@ -20,32 +20,6 @@ class davincicode {
     if (!(this.player1 == null) && !(this.player2 == null)) {
       console.log("player1 + player2: " + this.player1.id + " - " + this.player2.id);
   console.log("Spiel beginnen");
-  AblageListe[0] = [ [0,"black",false], [0,"white",false], [1,"black",false],[1,"white",false], [2,"black",false], [2,"white",false], [3,"black",false], [3,"white",false] , [4,"black",false], [4,"white",false] ,[5,"black",false], [5,"white",false], [6,"black",false], [6,"white",false] , [7,"black",false], [7,"white",false] , [8,"black",false] , [8,"white",false], [9,"black",false], [9,"white",false] , [10,"black",false] , [10,"white",false] , [11,"black",false] , [11,"white",false]];
-  for (i = AblageListe[0].length - 1; i > 0; i--) {
-    j = Math.floor(Math.random() * (i + 1));
-    x = AblageListe[0][i];
-    AblageListe[0][i] = AblageListe[0][j];
-    AblageListe[0][j] = x;
-  }
-  AblageListe[1] = [];
-  AblageListe[2] = [];
-  for (var i = 0; i < 4; i++) {
-    AblageListe[1].push(AblageListe[0][0])
-    AblageListe[0].shift();
-  }
-  for (var i = 0; i < 4; i++) {
-    AblageListe[2].push(AblageListe[0][0])
-    AblageListe[0].shift();
-  }
-
-  this.broadcast({
-  "type": "KartenSpieler1",
-  "data": AblageListe[1]
-});
-this.broadcast({
-"type": "KartenSpieler2",
-"data": AblageListe[2]
-});
 reihenfolge = [this.player1.id, this.player2.id];
 this.broadcast({
 "type": "Reihenfolge",
@@ -59,10 +33,10 @@ this.send(this.player2.client, {
   "type": "name",
   "data": reihenfolge[1]
 });
-this.broadcast({
-"type": "KartenZiehen",
-"data": AblageListe[0]
-});
+// this.broadcast({
+// "type": "KartenZiehen",
+// "data": AblageListe[0]
+// });
   }
 }
   onMessage(client, data) {
